b2a76290ccdf41ea57498a98fa6a28327a7a1ba3
Christopher Su <christophersu9@gmail.com>
2016-09-23 23:36:49 -0700

    stuff

--- json_to_plain_txt.py
+++ json_to_plain_txt.py
@@ -3,13 +3,22 @@ import sys
 
 with open('input.json') as data_file:    
   data = json.load(data_file)
+  items = {}
   for card in data['cards']:
-    item = '- ' + card['name']
-    for label in card['labels']:
-      item += ' #' + label['name'].replace(' ', '_').lower()
-    
-    print item.encode('utf-8').strip()
-    
-    if 'desc' in card and card['desc']:
-      for line in card['desc'].splitlines():
-        print '  ' + line.encode('utf-8').strip()
+    if card['idList'] not in items:
+      items[card['idList']] = []
+    items[card['idList']].append(card)
+
+  for key, value in items.iteritems():
+    print '- ' + key
+    indent = '  '
+    for card in value:
+      item = indent + '- ' + card['name'].strip()
+      for label in card['labels']:
+        item += ' #' + label['name'].replace(' ', '_').lower()
+      
+      print item.encode('utf-8')
+      
+      if 'desc' in card and card['desc']:
+        for line in card['desc'].splitlines():
+          print indent + '  ' + line.encode('utf-8')
